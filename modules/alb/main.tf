3bcbd0efd37d16b486bce951fbcb0723dae75efd
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-09 14:53:35 +0530

    alb-update

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -19,9 +19,25 @@ resource "aws_lb" "openproject_alb" {
 
 resource "aws_lb_target_group" "openproject_tg" {
   name     = "openproject-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/login"
+    port                = "8080"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "OpenProject-TG"
+  }
+  
 }
 
 resource "aws_lb_listener" "openproject_listener" {
@@ -38,5 +54,5 @@ resource "aws_lb_listener" "openproject_listener" {
 resource "aws_lb_target_group_attachment" "ec2_attach" {
   target_group_arn = aws_lb_target_group.openproject_tg.arn
   target_id        = var.instance_id
-  port             = 80
+  port             = 8080
 }
